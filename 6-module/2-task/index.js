2b8421b1c3345eb666e34b1bf740f614b999d8d2
Mihail-mos <d-misha_1990@mail.ru>
2026-01-19 21:30:03 +0300

    Lesson 6

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,43 @@
+import createElement from '../../assets/lib/create-element.js';
+
 export default class ProductCard {
   constructor(product) {
+    this.product = product;
+    this._elem = null;
+    this.render();
+  }
+
+  render() {
+    const formattedPrice = `€${this.product.price.toFixed(2)}`;
+    const imagePath = `/assets/images/products/${this.product.image}`;
+    
+    const cardHTML = `
+      <div class="card">
+        <div class="card__top">
+          <img src="${imagePath}" class="card__image" alt="${this.product.name}">
+          <span class="card__price">${formattedPrice}</span>
+        </div>
+        <div class="card__body">
+          <div class="card__title">${this.product.name}</div>
+          <button type="button" class="card__button">
+            <img src="/assets/images/icons/plus-icon.svg" alt="add">
+          </button>
+        </div>
+      </div>
+    `;
+    
+    this._elem = createElement(cardHTML);
+    
+    const button = this._elem.querySelector('.card__button');
+    button.addEventListener('click', () => {
+      this._elem.dispatchEvent(new CustomEvent('product-add', {
+        detail: this.product.id,
+        bubbles: true
+      }));
+    });
+  }
+
+  get elem() {
+    return this._elem;
   }
 }
--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -3,5 +3,113 @@ import createElement from '../../assets/lib/create-element.js';
 export default class Carousel {
   constructor(slides) {
     this.slides = slides;
+
+    this.currentSlide = 0;
+    this.elem = this.#createCarousel();
+    this.#initCarousel();
+  }
+
+  #createCarousel() {
+    const carouselHTML = `
+      <div class="carousel">
+        <div class="carousel__arrow carousel__arrow_right">
+          <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+        </div>
+        <div class="carousel__arrow carousel__arrow_left">
+          <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
+        </div>
+        <div class="carousel__inner">
+          ${this.slides.map(slide => this.#createSlideHTML(slide)).join('')}
+        </div>
+      </div>
+    `;
+    
+    return createElement(carouselHTML);
+  }
+
+  #createSlideHTML(slide) {
+    const formattedPrice = `€${slide.price.toFixed(2)}`;
+    const imagePath = `/assets/images/carousel/${slide.image}`;
+    
+    return `
+      <div class="carousel__slide" data-id="${slide.id}">
+        <img src="${imagePath}" class="carousel__img" alt="${slide.name}">
+        <div class="carousel__caption">
+          <span class="carousel__price">${formattedPrice}</span>
+          <div class="carousel__title">${slide.name}</div>
+          <button type="button" class="carousel__button">
+            <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+          </button>
+        </div>
+      </div>
+    `;
+  }
+
+  #initCarousel() {
+    this.#initControls();
+    this.#addEventListeners();
+    this.#updateControls();
+  }
+
+  #initControls() {
+    this.carouselInner = this.elem.querySelector('.carousel__inner');
+    this.arrowRight = this.elem.querySelector('.carousel__arrow_right');
+    this.arrowLeft = this.elem.querySelector('.carousel__arrow_left');
+    this.totalSlides = this.slides.length;
+  }
+
+  #addEventListeners() {
+    this.arrowRight.addEventListener('click', () => {
+      this.#nextSlide();
+    });
+
+    this.arrowLeft.addEventListener('click', () => {
+      this.#prevSlide();
+    });
+
+    this.elem.addEventListener('click', (event) => {
+      if (event.target.closest('.carousel__button')) {
+        this.#onAddButtonClick(event);
+      }
+    });
+  }
+
+  #nextSlide() {
+    if (this.currentSlide < this.totalSlides - 1) {
+      this.currentSlide++;
+      this.#updateCarousel();
+    }
+  }
+
+  #prevSlide() {
+    if (this.currentSlide > 0) {
+      this.currentSlide--;
+      this.#updateCarousel();
+    }
+  }
+
+  #updateCarousel() {
+    const slideWidth = this.carouselInner.offsetWidth;
+    const offset = -this.currentSlide * slideWidth;
+    this.carouselInner.style.transform = `translateX(${offset}px)`;
+    this.#updateControls();
+  }
+
+  #updateControls() {
+    this.arrowLeft.style.display = this.currentSlide === 0 ? 'none' : '';
+    
+    this.arrowRight.style.display = this.currentSlide === this.totalSlides - 1 ? 'none' : '';
+  }
+
+  #onAddButtonClick(event) {
+    const slideElement = event.target.closest('.carousel__slide');
+    const productId = slideElement.dataset.id;
+    
+    const customEvent = new CustomEvent('product-add', {
+      detail: productId,
+      bubbles: true
+    });
+    
+    this.elem.dispatchEvent(customEvent);
   }
 }
